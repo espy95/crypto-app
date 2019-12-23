a6dfb19da04266551a9d6643cb88a00febad52da
espy95 <olafseglajs95@gmail.com>
2019-12-23 21:37:01 +0200

    Assignment 1:  Text inputs + Layout

--- src/App.js
+++ src/App.js
@@ -1,51 +1,78 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-import { Button } from '@material-ui/core'
+import React, { useState } from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import Assignment1 from './Assignment1'
+import Assignment2 from './Assignment2'
 
-export default function App() {
+const pathMap = {
+  '/': 'Home',
+  '/assignment1': 'Assignment 1, block ciphers and chaining modes',
+  '/assignment2': 'Assignment 2, X.509 certificates'
+}
+
+export default function App () {
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/users'>Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/users'>
-            <Users />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Route
+        path='/'
+        render={({ location }) => (
+          <AppBar position='static' color='primary'>
+            <Toolbar>
+              <IconButton
+                edge='start'
+                color='inherit'
+                aria-controls='menu'
+                aria-haspopup='true'
+                aria-label='menu'
+                onClick={handleClick}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id='menu'
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose} component={Link} to='/assignment1'>Assignment1</MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to='/assignment2'>Assignment2</MenuItem>
+              </Menu>
+              <Typography>
+                {pathMap[window.location.pathname]}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        )}
+      />
+      <Switch>
+        <Route path='/assignment1'>
+          <Assignment1 />
+        </Route>
+        <Route path='/assignment2'>
+          <Assignment2 />
+        </Route>
+        <Route path='/'>
+          <Typography variant='h4' align='center'>Please select the Assignment from the Menu List</Typography>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
-}
-
-function Home() {
-  return <h2>Home</h2>
-}
-
-function About() {
-  return <h2>About</h2>
-}
-
-function Users() {
-  return <h2>Users</h2>
 }
---
+++ src/Assignment2.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function Assignment2 () {
+  return <Typography variant='h4' align='center'>Assignment 2, X.509 certificates</Typography>
+}

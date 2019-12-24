d6568bfa8ea78cc501e25030a200096d8d3a3c3c
espy95 <espy95@github.com>
2019-12-24 08:23:13 +0200

    Assignment 1: Tabs

--- src/App.js
+++ src/App.js
@@ -1,75 +1,21 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
-import {
-  AppBar,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Typography
-} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
-import Assignment1 from './Assignment1'
-import Assignment2 from './Assignment2'
-
-const pathMap = {
-  '/': 'Home',
-  '/assignment1': 'Assignment 1, block ciphers and chaining modes',
-  '/assignment2': 'Assignment 2, X.509 certificates'
-}
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Header from './Header'
+import Assignment1 from './Assignment1/Assignment1'
+import Assignment2 from './Assignment2/Assignment2'
+import { Typography } from '@material-ui/core'
 
 export default function App () {
-  const [anchorEl, setAnchorEl] = useState(null)
-
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
   return (
     <BrowserRouter>
       <Route
         path='/'
-        render={({ location }) => (
-          <AppBar position='static' color='primary'>
-            <Toolbar>
-              <IconButton
-                edge='start'
-                color='inherit'
-                aria-controls='menu'
-                aria-haspopup='true'
-                aria-label='menu'
-                onClick={handleClick}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id='menu'
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose} component={Link} to='/assignment1'>Assignment1</MenuItem>
-                <MenuItem onClick={handleClose} component={Link} to='/assignment2'>Assignment2</MenuItem>
-              </Menu>
-              <Typography>
-                {pathMap[window.location.pathname]}
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        )}
+        render={() => <Header />}
       />
       <Switch>
-        <Route path='/assignment1'>
-          <Assignment1 />
-        </Route>
-        <Route path='/assignment2'>
-          <Assignment2 />
-        </Route>
-        <Route path='/'>
+        <Route path='/assignment1' component={Assignment1} />
+        <Route path='/assignment2' component={Assignment2} />
+        <Route exact path='/'>
           <Typography variant='h4' align='center'>Please select the Assignment from the Menu List</Typography>
         </Route>
       </Switch>
---
+++ src/Header.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const pathMap = {
+  '/': 'Home',
+  '/assignment1': 'Assignment 1, block ciphers and chaining modes',
+  '/assignment2': 'Assignment 2, X.509 certificates'
+}
+
+export default function Header () {
+  const location = useLocation()
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <AppBar position='static' color='primary'>
+      <Toolbar>
+        <IconButton
+          edge='start'
+          color='inherit'
+          aria-controls='menu'
+          aria-haspopup='true'
+          aria-label='menu'
+          onClick={handleClick}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id='menu'
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose} component={Link} to='/assignment1'>Assignment1</MenuItem>
+          <MenuItem onClick={handleClose} component={Link} to='/assignment2'>Assignment2</MenuItem>
+        </Menu>
+        <Typography>
+          {pathMap[location.pathname]}
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
